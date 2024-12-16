f277760cc40e78de48315a53a253571cfd20efc4
JH0nnYGb <silvajoneffe@gmail.com>
2024-12-16 09:34:36 -0300

    configuração da projeto - Catalogo de jogos GEjif

--- app_games_catalog/urls.py
+++ app_games_catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app_games_catalog import games_posts_views
+from app_games_catalog.views import games_posts_views
 
+app_name = 'app_games_catalog'
+ 
 urlpatterns = [
     path('',games_posts_views,name='games_posts'),
     path('Catalogo/',games_posts_views,name='games_posts'),
